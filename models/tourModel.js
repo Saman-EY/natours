ae34cbe2409e0c359fb128548fb3939453d931a9
Saman <samanezzatabadi@yahoo.com>
2025-07-22 11:55:39 +0330

    query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -97,6 +97,13 @@ tourSchema.pre('save', function(next) {
 // tourSchema.pre('find', function(next) {
 tourSchema.pre(/^find/, function(next) {
   this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(next) {
+  console.log(`query took ${Date.now() - this.start} milliseconds!`);
   next();
 });
 
