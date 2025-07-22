58199bccbb0bbe30a4bcf6748d386583c5fe5b23
Saman <samanezzatabadi@yahoo.com>
2025-07-22 21:11:09 +0330

    handling error for 404 err

--- app.js
+++ app.js
@@ -46,4 +46,29 @@ app.use('/api/v1/users', userRouter);
 // app.patch('/api/v1/tours/:id', patchTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `can't find the ${req.originalUrl} in this website :)`
+  // });
+
+  const err = new Error(`can't find the ${req.originalUrl} in this website :)`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+
+  next();
+});
+
 module.exports = app;
