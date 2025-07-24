08d0788d160d4a596e43cb8d781b6f58ae6d417b
Saman <samanezzatabadi@yahoo.com>
2025-07-24 10:29:47 +0330

    handle operational error - dev and prod error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,37 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+
+    // don't leak error to the client
+  } else {
+    console.error('Error: ', err);
+
+    res.status(500).json({
+      status: 'fail',
+      message: 'something went very wrong'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-
-  next();
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
