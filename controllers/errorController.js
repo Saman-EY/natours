85c004c3507c7e09eff8195df1efeb4ca4bce1a1
Saman <samanezzatabadi@yahoo.com>
2025-07-25 23:44:13 +0330

    validation error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -39,6 +39,14 @@ const handleDuplicateFieldsDB = error => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join(". ")}`;
+
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -50,6 +58,8 @@ module.exports = (err, req, res, next) => {
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
